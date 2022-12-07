32dc305382c1508176d7f1d4a319844140393588
adriennecordell <adrienne.cordellac@gmail.com>
2022-12-06 20:14:06 -0600

    updated port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3303;
+const PORT = process.env.PORT || 5000;
 
 const sess = {
     secret: 'secret',
