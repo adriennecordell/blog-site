3755e94c68f86f60ce269702208e9151a163a80b
adriennecordell <110563204+adriennecordell@users.noreply.github.com>
2022-12-29 21:35:51 -0600

    added routes to posts

--- controllers/view-routes.js
+++ controllers/view-routes.js
@@ -1,36 +1,101 @@
 const router = require('express').Router();
-const{ post, User } = require('../models')
+const { Post, User, Comment } = require('../models');
 
-router.get('/', async (req, res) => {
-    try {
-        let posts = await post.findAll()
-        posts = posts.map(post => post.get({ plain: true }))
-        res.render('home', {
-            projects,
-            logged_in: res.session.logged_in
+// gets all posts for the homepage
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+    
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    }); 
+});
+
+// get a single post
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_content',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            const post = dbPostData.get({ plain: true });
+
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
         })
-    } catch(err) {
-        res.status(500).json(err)
-    }
-})
-
-router.get('/project/:id', async (req, res) => {
-    try {
-        let post = await post.findByPk(req.params.id)
-        project = post.get({ plain: true })
-
-        res.render('post', {
-            post, 
-            logged_in: req.session.logged_in
-        })
-
-    } catch(err) {
-        res.status(500).json(err)
-    }
-})
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 router.get('/login', (req, res) => {
-    res.render('login')
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
 })
 
 module.exports = router;
