4f472f8361abd30111fc144d63f33ad3da25a58f
adriennecordell <adrienne.cordellac@gmail.com>
2023-01-01 20:18:17 -0600

    fixed small errors

--- models/User.js
+++ models/User.js
@@ -29,11 +29,11 @@ User.init(
         },
     }, {
         hooks: {
-            async beforeCreate(newUserData) => {
+            async beforeCreate(newUserData)  {
                 newUserData.password = await bcrypt.hash(newUserData.password, 10);
                 return newUserData;
             },
-            async beforeUpdate(updatedUserData) => {
+            async beforeUpdate(updatedUserData) {
                 updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
                 return updatedUserData;
             },
